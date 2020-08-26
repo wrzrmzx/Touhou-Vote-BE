a9f66e95657bd924c6506877b579279f525d1909
zyddnys <zyddnys@outlook.com>
2020-08-26 16:07:25 -0400

    add validator url

--- gateway/src/services.rs
+++ gateway/src/services.rs
@@ -2,3 +2,4 @@
 pub const USER_MANAGER: &'static str = "127.0.0.1:1100";
 pub const SUBMIT_HANDLER: &'static str = "127.0.0.1:1101";
 pub const RESULT_QUERY: &'static str = "127.0.0.1:1102";
+pub const SUBMIT_VALIDATOR: &'static str = "127.0.0.1:1103";
